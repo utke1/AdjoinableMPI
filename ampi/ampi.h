eb6404453456e3ba451ae1024e26ae7714c19684
utke on macbook <none@none>
2012-11-06 16:04:16 -0600

    spelling

--- ampi/ampi.h
+++ ampi/ampi.h
@@ -14,7 +14,7 @@ int AMPI_Send(void* buf,
 	      MPI_Comm comm);
 
 int AMPI_Recv(void* buf, 
-	      int cont,
+	      int count,
 	      MPI_Datatype datatype, 
 	      char isActive,
 	      int src, 
@@ -22,7 +22,7 @@ int AMPI_Recv(void* buf,
 	      MPI_Comm comm,
 	      MPI_Status* status);
 
-int AMPI_isend (void* buf, 
+int AMPI_Isend (void* buf, 
 		int count, 
 		MPI_Datatype datatype, 
 		char isActive,
@@ -31,7 +31,7 @@ int AMPI_isend (void* buf,
 		MPI_Comm comm, 
 		MPI_Request* request);
 
-int AMPI_irecv (void* buf, 
+int AMPI_Irecv (void* buf, 
 		int count, 
 		MPI_Datatype datatype, 
 		char isActive,
@@ -43,15 +43,15 @@ int AMPI_irecv (void* buf,
 int AMPI_Wait(MPI_Request *request, 
 	      MPI_Status *status);
 
-int AMPI_waitall (int count, 
+int AMPI_Waitall (int count, 
 		  MPI_Request requests[], 
 		  MPI_Status statuses[]);
 
-int AMPI_awaitall (int count, 
+int AMPI_Awaitall (int count, 
 		   MPI_Request requests[], 
 		   MPI_Status statuses[]);
 
-int AMPI_reduce (void* sbuf, 
+int AMPI_Reduce (void* sbuf, 
 		 void* rbuf, 
 		 int count, 
 		 MPI_Datatype datatype, 
